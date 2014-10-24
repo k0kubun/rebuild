037755b14156cd1de7d4afd600b577294706523b
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-24 17:20:51 +0900

    Show usage

--- lib/rebuild/cli.rb
+++ lib/rebuild/cli.rb
@@ -1,9 +1,23 @@
 require 'rebuild'
+require 'unindent'
 
 module Rebuild
   class CLI
-    def self.run
-      puts "run"
+    class << self
+      def run
+        return show_usage if ARGV.empty?
+      end
+
+      private
+
+      def show_usage
+        puts <<-EOS.unindent
+          Commands:
+            rebuild              # install command line tools
+            rebuild USER/PROJECT # execute all scripts in GitHub repo's rebuild directory
+
+        EOS
+      end
     end
   end
 end
