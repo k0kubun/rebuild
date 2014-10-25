4b159bd3b3601b5d51248ef89a9fa38af157dae0
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-26 01:06:00 +0900

    Support iTerm2

--- lib/rebuild/command_line_tools.rb
+++ lib/rebuild/command_line_tools.rb
@@ -2,8 +2,11 @@ require 'rebuild'
 
 module Rebuild
   class CommandLineTools
-    DATABASE          = '/Library/Application\ Support/com.apple.TCC/TCC.db'
-    BUNDLE_IDENTIFIER = 'com.apple.Terminal'
+    DATABASE            = '/Library/Application\ Support/com.apple.TCC/TCC.db'
+    SUPPORTED_TERMINALS = %w[
+      com.apple.Terminal
+      com.googlecode.iterm2
+    ]
 
     class << self
       def installed?
@@ -23,12 +26,14 @@ module Rebuild
 
       # Enable Security & Privacy > Privacy > Accessibility for Terminal.app.
       def obtain_accesibility
-        puts "Require accessibility for Terminal.app to click buttons"
-        sql = <<-SQL
-          INSERT OR REPLACE INTO access
-          VALUES('kTCCServiceAccessibility','#{BUNDLE_IDENTIFIER}',0,1,0,NULL);
-        SQL
-        `sudo sqlite3 #{DATABASE} "#{sql}"`
+        puts 'Require accessibility for terminal to click buttons'
+        SUPPORTED_TERMINALS.each do |bundle_identifier|
+          sql = <<-SQL
+            INSERT OR REPLACE INTO access
+            VALUES('kTCCServiceAccessibility','#{bundle_identifier}',0,1,0,NULL);
+          SQL
+          `sudo sqlite3 #{DATABASE} "#{sql}"`
+        end
       end
 
       def execute_scpt(name)
